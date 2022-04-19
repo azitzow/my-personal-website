c324663cbf83b5638610f5a5dc3425cbd392526c
Ana Zitzow <anasempai89@gmail.com>
2022-04-18 20:23:09 -0700

    finished product

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 // JS code
-// document.addEventListener('DOMContentLoaded', function () {
-//   const button = document.getElementsByClassName('btn');
-
-//   const handleClick = function () {
-//     alert('Welcome to my first website');
-//   };
-
-//   const logo = document.querySelector('.logo');
-
-//   logo.addEventListener('click', handleClick);
-// });
 const button = document.querySelector('.btn');
 
-const handleClick = function (bob) {
-  console.log(bob);
+const handleClick = function () {
   alert('Welcome to my first website');
 };
 
