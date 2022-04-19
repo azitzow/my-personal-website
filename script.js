abb06c0b4641d03b5bc4fba59e05c76cc689f382
Ana Zitzow <anasempai89@gmail.com>
2022-04-18 20:10:26 -0700

    two event listeners added

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // JS code
+// document.addEventListener('DOMContentLoaded', function () {
+//   const button = document.getElementsByClassName('btn');
+
+//   const handleClick = function () {
+//     alert('Welcome to my first website');
+//   };
+
+//   const logo = document.querySelector('.logo');
+
+//   logo.addEventListener('click', handleClick);
+// });
+const button = document.querySelector('.btn');
+
+const handleClick = function (bob) {
+  console.log(bob);
+  alert('Welcome to my first website');
+};
+
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', handleClick);
+button.addEventListener('click', handleClick);
+
